6e80acc354d060220bbf5ef33a4b17684b477c3c
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2023-10-30 22:23:12 +0700

    add 234. palindrome linedlist

---
+++ src/LinkedList/PalindromeLinkedList.java
@@ -0,0 +1,52 @@
+package LinkedList;
+
+public class PalindromeLinkedList {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        ListNode middle = findMiddle(head);
+        ListNode reversedHalf = reverseList(middle.next);
+        middle.next = null;
+
+        ListNode p1 = head;
+        ListNode p2 = reversedHalf;
+
+        while (p1 != null && p2 != null) {
+            if (p1.val != p2.val) {
+                return false;
+            }
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        return true;
+    }
+
+    private ListNode findMiddle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+}
