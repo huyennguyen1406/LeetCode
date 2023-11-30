67fa44328eb7f3cbc80be6b739f9f9edf48dc01f
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2023-11-30 23:40:55 +0700

    137. Single Number 2

---
+++ src/Array/SingleNumber2.java
@@ -0,0 +1,12 @@
+package Array;
+
+public class SingleNumber2 {
+    public int singleNumber(int[] nums) {
+        int seenOnce = 0, seenTwice = 0;
+        for (int num : nums) {
+            seenOnce = ((seenOnce ^ num) & (~seenTwice));
+            seenTwice = ((seenTwice ^ num) & (~seenOnce));
+        }
+        return seenOnce;
+    }
+}
